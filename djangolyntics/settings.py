3862e1593e58e9e45b26f02378653488b86a9097
Douglas Anderson <dougjamesanderson@gmail.com>
2014-10-22 15:04:42 -0400

    Tweaked the settings to reflect my local development postgres database.

--- djangolyntics/settings.py
+++ djangolyntics/settings.py
@@ -59,13 +59,11 @@ WSGI_APPLICATION = 'djangolyntics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
+DATABASES = {
+    'default': dj_database_url.config(
+        default="postgres://django:django@localhost/djangolyntics"
+        )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
