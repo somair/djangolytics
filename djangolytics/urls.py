6f24631c5e95298d16ac9b52b62691e18e26ac36
Douglas Anderson <dougjamesanderson@gmail.com>
2014-10-23 18:31:06 -0400

    Woops the links on the index did not match the app's urls

--- djangolytics/urls.py
+++ djangolytics/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', "googleAnalytics.views.index"),
     url(r'^hit_api$', "googleAnalytics.views.hit_api"),
-    url(r'^date_picker$', "googleAnalytics.views.select_date"),
+    url(r'^pick_date$', "googleAnalytics.views.select_date"),
     url(r'^dot_chart$', "googleAnalytics.views.dot_chart"),
     url(r'^oauth2callback$', "googleAnalytics.views.auth_return"),
 
