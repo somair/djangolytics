8adf5533bc01bd50b4954ce7d21eea74fa305e0f
Douglas Anderson <dougjamesanderson@gmail.com>
2014-10-24 10:54:55 -0400

    Attempting validation between fields in the form

--- googleAnalytics/forms.py
+++ googleAnalytics/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 DATE_HELP_STR = "Dates must be of the form YYYY-MM-DD"
+
+def date_order_validator(start_date, end_date):
+    """Throws a validation error if the end date is before the start date"""
+    if start_date < end_date:
+        raise ValidationError("End date cannot be before Start date",
+                code="date order"):
 
 class StartEndDateForm(forms.Form):
     start_date = forms.DateField(label="Start date:",
@@ -10,3 +17,12 @@ class StartEndDateForm(forms.Form):
     end_date = forms.DateField(label="End date:",
                                required=True,
                                input_formats=["%Y-%m-%d"])
+
+    def clean(self):
+        """Perform validation between fields"""
+        # Perform parent validations
+        cleaned_data = super(StartEndDateForm, self).clean()
+        clean_start = cleaned_data["start_date"]
+        clean_end = cleaned_data["start_end"]
+        date_order_validator(clean_start, clean_end)
+
