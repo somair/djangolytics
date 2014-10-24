be4e44194d875389a2cf1822139aca2846359ced
Douglas Anderson <dougjamesanderson@gmail.com>
2014-10-24 17:23:45 -0400

    Adding a route for logout

--- djangolytics/urls.py
+++ djangolytics/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', "django.contrib.auth.views.login",
                               {"template_name": "googleAnalytics/login.html"}),
+    url(r'^accounts/logout/$', "django.contrib.auth.views.logout"),
 )
