0ae129b301ffbcbed4c6b732b743880346845d32
Douglas Anderson <dougjamesanderson@gmail.com>
2014-10-24 09:57:49 -0400

    Created a representation function for the sake of printing the database results

--- googleAnalytics/models.py
+++ googleAnalytics/models.py
@@ -13,6 +13,10 @@ class HourlyDataModel(models.Model):
     date = models.DateField("The date that the session occurred")
     hour = models.IntegerField("The hour that the session occurred")
     #TODO consider subclassing to add min value and max value
-    #TODO use unique for date
     num_sessions = models.IntegerField("The number of sessions")
     #TODO consider subclassing to add min value
+
+    def __unicode__():
+        formattable_str = "date:{}  hour:{}  number of sessions:{}"
+        return formattable_str.format(self.date, self.hour, self.num_sessions)
+
